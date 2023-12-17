0f6f6eee7a913cc93715e85ad6d42db0764fd89b
VivekThazhathattil <tvivek@iisc.ac.in>
2023-12-17 13:27:09 +0530

    cf+

--- codeforces/1553A.c
+++ codeforces/1553A.c
@@ -1,16 +1,60 @@
 #include <stdio.h>
 
+int leading_digit(int n){
+  int digit = 0;
+  while(n){
+    digit = n%10;
+    n /= 10;
+  }
+  return digit;
+}
+
+int get_num_digits(int n){
+  int num = 0;
+  while(n){
+    ++num;
+    n /= 10;
+  }
+  return num;
+}
+
+int residual_num(int n, int ld, int ndigs){
+  int m = 1; 
+  while(--ndigs){
+    m *= 10;
+  }
+  return n - ld*m;
+}
+
+int get_mult(int n){
+  int mult = 1;
+  while(n){
+    mult *= 10;
+    --n;
+  }
+  return mult;
+}
+
+int interesting_nums(int n){
+  if(n < 9){
+    return 0;
+  }
+  else if(n == 9){
+    return 1;
+  }
+  int ld = leading_digit(n);
+  int num_digits = get_num_digits(n);
+  n = residual_num(n, ld, num_digits);
+  return ld * get_mult(num_digits - 2) + interesting_nums(n);
+}
+
 int main(){
   int t;
   scanf("%d", &t);
   while(t--){
     int n;
     scanf("%d", &n);
-    int mult = 10, num = 0;
-    while(mult <= n){
-      num += 9*(mult/100);
-      mult *= 10;
-    }
+    int num = interesting_nums(n);
     printf("%d\n", num);
   }
   return 0;
